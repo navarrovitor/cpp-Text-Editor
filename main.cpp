ed25524d2e72573b0b1b92650aff4b4b6e495f8e
vitor navarro <vitor.san.nav.nexus@gmail.com>
2021-05-31 13:54:27 -0300

    update git ignore, clean up main.cpp and prepare it for the menu

--- main.cpp
+++ main.cpp
@@ -5,24 +5,18 @@
 int main()
 {
   LinkedList linkedList;
-
+  int option;
   string file = "inteligenciaEmocional.txt";
 
   char character;
-
   fstream textFileIn(file);
-
   while (textFileIn >> noskipws >> character)
-  {
     linkedList.insertAtTail(character);
-  }
   textFileIn.close();
 
   fstream textFileOut(file, ios::out | ios::trunc);
   while (!linkedList.empty())
-  {
     textFileOut.put(linkedList.removeHead());
-  }
 
   textFileOut.close();
   return 0;
